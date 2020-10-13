f813df7b30c5c18ca6492c3b356a06285e1f2450
italoucsp <42581127+italoucsp@users.noreply.github.com>
2020-10-13 15:21:35 -0500

    shell en c

--- TAREA LAB SHELL/shell2.c
+++ TAREA LAB SHELL/shell2.c
@@ -13,9 +13,9 @@
 
 char history_queue[HISTORY_SIZE][MAX_LINE];
 int history_len[HISTORY_SIZE];
-int n_hcmd = 0, posin, posout;
+int n_hcmd = 0, posin, posout, pfile;
 
-void saveCmdsInHistory(char *command, char input[], int length){
+void saveCmdsInHistory(char *command, char input[], int length) {
     if (strcmp(command,"history")) {
         int i;
         for (i = n_hcmd; i > 0; i--) {
@@ -31,7 +31,7 @@ void saveCmdsInHistory(char *command, char input[], int length){
     }
 }
 
-void showHistory(){
+void showHistory() {
     if (n_hcmd) {
         printf("Commands History:\n");
         int i, j;
@@ -48,16 +48,14 @@ void showHistory(){
     }
 }
 
-void dup2_file_in(char *commands[]){
-    commands[posin] = NULL;
-    int file = open(commands[posin + 1], O_RDONLY);
-    dup2(file, 0);
+void dup2_file_in(char *commands[]) {
+    pfile = open(commands[posin + 1], O_RDONLY);
+    dup2(pfile, 0);
 }
 
-void dup2_file_out(char *commands[]){
-    commands[posout] = NULL;
-    int file = open(commands[posout + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG);
-    dup2(file, 1);
+void dup2_file_out(char *commands[]) {
+    pfile = open(commands[posout + 1], O_WRONLY | O_CREAT | O_TRUNC);
+    dup2(pfile, 1);
 }
 
 int readInput(char *commands[], char input[], int length) {
@@ -84,7 +82,7 @@ int readInput(char *commands[], char input[], int length) {
     int i, ci = 0, cmd_flag = -1;
     commands[ci] = &input[0];
     for (i = 1; i < length; i++){
-        if (input[i] == ' '){
+        if (input[i] == ' ' || input[i] == '\t'){
             input[i] = '\0';
         }
         else if (input[i] == '&'){
@@ -93,12 +91,14 @@ int readInput(char *commands[], char input[], int length) {
                 cmd_flag = 1;
         }
         else if (input[i] == '>'){
+            input[i] = '\0';
             posout = ci + 1;
             if (cmd_flag == -1)
                 cmd_flag = 2;
         }
         else if (input[i] == '<'){
             posin = ci + 1;
+            input[i] = '\0';
             if (cmd_flag == -1)
                 cmd_flag = 3;
         }
@@ -106,7 +106,7 @@ int readInput(char *commands[], char input[], int length) {
             if (cmd_flag == -1)
                 cmd_flag = 4;
         }
-        if (!input[i - 1]){
+        if (!input[i - 1] && input[i]){
             commands[++ci] = &input[i];
         }
     }
@@ -119,14 +119,64 @@ int readInput(char *commands[], char input[], int length) {
     return cmd_flag;
 }
 
+void splitCmds(char *commands[], char *cmd1[], char *cmd2[]){
+    int i;
+    for (i = 0; i < CMDS_N; i++){
+        if(!strcmp(commands[i],"|"))
+            break;
+        cmd1[i] = commands[i];
+    }
+    cmd1[i] = NULL;
+    ++i;
+    int j;
+    for (j = 0; i < CMDS_N; i++){
+        if(!commands[i])
+            break;
+        cmd2[j] = commands[i];
+        ++j;
+    }
+    cmd2[j] = NULL;
+}
+
+void executePipe(char *commands[]) {
+    char *cmd1[CMDS_N], *cmd2[CMDS_N];
+    splitCmds(commands, cmd1, cmd2);
+
+    int childs_pipe[2];
+    pid_t s_child1, s_child2;
+    pipe(childs_pipe);
+    s_child1 = fork();
+    bool inp = 1;
+    if (s_child1 == 0) {
+        dup2(childs_pipe[inp], STDIN_FILENO);
+        close(childs_pipe[!inp]);
+        if (execvp(cmd1[0], cmd1) == -1) {
+            printf("Error executing the command.\n");
+        }
+    }
+    s_child2 = fork();
+    if (s_child2 == 0) {
+        dup2(childs_pipe[!inp], STDIN_FILENO);
+        close(childs_pipe[inp]);
+        if (execvp(cmd2[0], cmd2) == -1) {
+            printf("Error executing the command.\n");
+        }
+    }
+    close(childs_pipe[!inp]);
+    close(childs_pipe[inp]);
+    wait(NULL);
+}
+
 int main(void) {
     bool should_run = true;
     char input[MAX_LINE];
     char *commands[CMDS_N];
     pid_t child_pid;
-    int stat_loc, fin, fout;
+    int fin, fout;
+    fin = fout = dup(STDIN_FILENO);
 
     while (should_run) {
+        pfile = 0;
         dup2(fin, STDIN_FILENO);
         dup2(fout, STDIN_FILENO);
         printf("oki>> ");
@@ -136,18 +186,25 @@ int main(void) {
         if(cmd_flag){
             child_pid = fork();
             if (child_pid == 0) {
-                if (cmd_flag == 2){
+                if (cmd_flag == 2) {
                     dup2_file_out(commands);
+                    close(pfile);
                 }
-                else if (cmd_flag == 3){
+                else if (cmd_flag == 3) {
                     dup2_file_in(commands);
+                    close(pfile);
+                }
+                else if (cmd_flag == 4) {
+                    executePipe(commands);
+                    return 0;
                 }
                 if (execvp(commands[0], commands) == -1) {
                     printf("Error executing the command.\n");
                 }
+                return 0;
             } 
             else {
-                if (cmd_flag == 1) waitpid(child_pid, &stat_loc, WUNTRACED);
+                if (cmd_flag == 1) wait(NULL);
             }
         }
     }
